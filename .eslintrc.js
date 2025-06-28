e6bc063a0f51f0ddb18f7d891c36ba413d0a1ebc
GlennHuckett82 <gph1982@icloud.com>
2025-06-28 12:51:42 +0100

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  parser: '@babel/eslint-parser', // Updated from babel-eslint
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off', // Not needed with React 17+
+  },
+};
+
